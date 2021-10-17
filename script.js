c7d63e1143579e371da5b32460906871e3084925
MrDalo <dadokralik@gmail.com>
2021-10-17 16:01:42 +0200

    Before triggered side bars, cursor: pointer; is add

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 /*Animaciu side barov sprav tak, ze arrow a burger menu ostane len, ostatne das do display:none a zmenis pozadie, bdue sa lepsie robit animacia 
 a nebude s tym problem. Dokazes spravit jedno burger menu a jednu arrow in circle*/
+
+function getData(){
+
+   return false;
+}
+
+
+function createNewTask(e){
+    console.log(e);
+}
+
+function settingsOfMatrix(){
+
+}
+
+
+function burgerMenuTrigger(){
+    document.getElementById("leftMenu").classList.toggle("triggered");
+}
+
+function arrowCircleTrigger(){
+    document.getElementById("rightMenu").classList.toggle("triggered");
+
+}
+
+
+
+console.log(document.getElementById("taskComplete"));
